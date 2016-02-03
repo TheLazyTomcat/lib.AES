a66d0f5eb4a5392a74fdd5ba0f4ba494f22c0060
František Milt <frantisek.milt@gmail.com>
2016-02-03 13:10:00 +0100

    Added copyright info

--- AES.pas
+++ AES.pas
@@ -1,3 +1,22 @@
+{-------------------------------------------------------------------------------
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-------------------------------------------------------------------------------}
+{===============================================================================
+
+  Rijndael/AES cipher
+
+  ©František Milt 2016-02-03
+
+  Version 1.0
+
+  All combinations of allowed key and block sizes are implemented and should be
+  compatible with reference Rijndael cipher.
+
+===============================================================================}
 unit AES;
 
 {$IFDEF ENDIAN_BIG}
